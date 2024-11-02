d8a176acbb97a475a1d285a577aecd03ff8b8ec0
Osama_Alfaqeeh <osa20200503@std.psut.edu.jo>
2024-11-02 16:10:42 +0300

    Adding models and connecting to database

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'voting_system',
+        'USER': 'root',
+        'PASSWORD': 'MyDatabase$1234',
+        'HOST': 'localhost',   # Or the IP address if it's hosted elsewhere
+        'PORT': '3306',        # Default MySQL port
     }
 }
 
